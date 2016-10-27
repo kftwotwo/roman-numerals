74881e15146cf88c6a1a36cc9f7f05705536fb68
Kevin Finley and Nigel Burnett <kevin+nigel@>
2016-10-27 09:48:00 -0700

    add var to javascript

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+var numeralCodes = [["","I","II","III","IV","V","VI","VII","VIII","IX"],         // Ones
+                    ["","X","XX","XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],   // Tens
+                    ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"],// Hundreds
+                    ["","M","MM", "MMM",]];//thousands
+//////////////////////////////////////////////////////////////////////
+var nunber = "";
+
+
+
+
+
+
+////////////////////////////////////////////
+$(document).ready(function) {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+  }
+}
