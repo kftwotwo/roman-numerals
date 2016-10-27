cc5fef57148d5d9d616001d1a4e6f86fd25c7d8f
Kevin Finley and Nigel Burnett <kevin@>
2016-10-27 12:03:08 -0700

    add sythax to javascripts and worksgit add .git add .

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,22 @@
-var numeralCodes = [["","I","II","III","IV","V","VI","VII","VIII","IX"],         // Ones
-                    ["","X","XX","XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],   // Tens
-                    ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"],// Hundreds
-                    ["","M","MM", "MMM",]];//thousands
-//////////////////////////////////////////////////////////////////////
-var nunber = "";
+$(document).ready(function() {
 
-
-
-
-
-
-////////////////////////////////////////////
-$(document).ready(function) {
-  $("form").submit(function(event) {
+    $("form").submit(function(event) {
     event.preventDefault();
-
+    var userInput = parseInt($("#number-input").val());
+    convert(userInput);
+    })
+///////////////////////////////////////////////////////////////////////////////////////////
+var numeralCodes = [["","I","II","III","IV","V","VI","VII","VIII","IX"],         //  9 ones
+                    ["","X","XX","X", "XL", "L", "LX", "LXX", "LXXX", "XC"],   //  90 tens
+                    ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"],// 900 hundreds
+                    ["","M","MM", "MMM",]];// 3 thousand
+////////////////////////////////////////////////////////////////////////////////////////////
+  function convert(num) {
+    var numeral = "";
+    var digits = num.toString().split('').reverse();
+    for (var i=0; i < digits.length; i++){
+      numeral = numeralCodes[i][parseInt(digits[i])] + numeral;
+    }
+    $("#result").text(numeral);
   }
-}
+})
